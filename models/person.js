5c874d63cd8a901ad6a4910359393d16ecc93c6e
mohamedisakr <mohamedisakr@gmail.com>
2021-10-03 13:14:47 +0200

    adding validation to person model

--- models/person.js
+++ models/person.js
@@ -6,7 +6,7 @@ const {Schema, model} = mongoose
 
 const personSchema = new Schema({
   name: {type: String, unique: true, minlength: 3, required: true},
-  number: {type: String, minlength: 3, required: true},
+  number: {type: String, minlength: 8, required: true},
 })
 
 personSchema.set('toJSON', {
