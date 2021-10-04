1e02cb582a2eafa37f41dbdaf3736b19ef41db5e
mohamedisakr <mohamedisakr@gmail.com>
2021-10-04 10:04:56 +0200

    implementing exercises 4.4 step2

--- tests/list-helper.spec.js
+++ tests/list-helper.spec.js
@@ -1,7 +1,28 @@
-const {dummy} = require('../utils/list-helper')
+const {dummy, countTotalLikes} = require('../utils/list-helper')
+const {listWithOneBlog, blogs} = require('../fixtures/blogs.data')
 
-test('dummy returns one', () => {
-  const blogs = []
-  const result = dummy(blogs)
-  expect(result).toBe(1)
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+    const result = dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
+
+describe('total likes', () => {
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = countTotalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list is empty, equals 0', () => {
+    const emptyBlogs = []
+    const result = countTotalLikes(emptyBlogs)
+    expect(result).toBe(0)
+  })
+
+  test('when large list, get the right totals', () => {
+    const result = countTotalLikes(blogs)
+    expect(result).toBe(36)
+  })
 })
--- utils/list-helper.js
+++ utils/list-helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy,
+/**
+ * returns the total sum of likes in all of the blog posts.
+ * @param {*} blogs a list of blog posts
+ * @returns
+ */
+const countTotalLikes = (blogs) => {
+  const total = blogs.reduce(reducer, 0)
+  return total
 }
+
+// helpers
+const reducer = (sum, blog) => sum + blog.likes
+
+module.exports = {dummy, countTotalLikes}
