e14c6540e9af6407b6eca2ec7dec65d5a381c1dd
mohamedisakr <mohamedisakr@gmail.com>
2021-10-07 07:36:20 +0200

    adding error handling caused by TokenExpiredError

--- controllers/login.js
+++ controllers/login.js
@@ -18,7 +18,8 @@ loginRouter.post('/', async (request, response) => {
 
   const userForToken = {username: user.username, id: user._id}
 
-  const token = jwt.sign(userForToken, process.env.SECRET)
+  // token expires in 60*60 seconds, that is, in one hour
+  const token = jwt.sign(userForToken, process.env.SECRET, {expiresIn: 60 * 60})
 
   response.status(200).send({token, username: user.username, name: user.name})
 })
