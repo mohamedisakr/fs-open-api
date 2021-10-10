f7d1a51bf708a7611ca91e8ff09709f7d7097615
mohamedisakr <mohamedisakr@gmail.com>
2021-10-10 12:38:07 +0200

    adding endpoints url to env file

--- utils/config.js
+++ utils/config.js
@@ -11,4 +11,25 @@ const HOST = process.env.HOST
 const DB_PORT = process.env.DB_PORT
 const DATABASE_NAME = process.env.DATABASE_NAME
 
-module.exports = {MONGODB_URI, PORT, PROTOCOL, HOST, DB_PORT, DATABASE_NAME}
+// endpoint url
+const BASE_URL = process.env.BASE_URL
+const BLOG_URL = process.env.BLOG_URL
+const LOGIN_URL = process.env.LOGIN_URL
+const NOTE_URL = process.env.NOTE_URL
+const PERSON_URL = process.env.PERSON_URL
+const USER_URL = process.env.USER_URL
+
+module.exports = {
+  MONGODB_URI,
+  PORT,
+  PROTOCOL,
+  HOST,
+  DB_PORT,
+  DATABASE_NAME,
+  BASE_URL,
+  BLOG_URL,
+  LOGIN_URL,
+  NOTE_URL,
+  PERSON_URL,
+  USER_URL,
+}
