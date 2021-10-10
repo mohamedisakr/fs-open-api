66029deeb96ceb7d38f7c01b55caf10efe018dff
mohamedisakr <mohamedisakr@gmail.com>
2021-10-10 13:19:20 +0200

    refactoring person test cases

--- __tests__/person-api.test.js
+++ __tests__/person-api.test.js
@@ -3,16 +3,17 @@ const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
+const config = require('../utils/config')
 const url = '/api/persons'
 const info = '/api/info'
 
 describe('endpoint', () => {
   test('should get persons info - # of persons', async () => {
-    await api.get(info).expect(200)
+    await api.get(config.INFO_URL).expect(200)
   })
 
   test('should get all persons', async () => {
-    await api.get(url).expect(200)
+    await api.get(config.PERSON_URL).expect(200)
     //   .expect('Content-Type', /application\/json/)
   })
 })
