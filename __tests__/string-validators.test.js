bfab117f64193aa8a55c3ad615055ab1a018c739
mohamedisakr <mohamedisakr@gmail.com>
2021-10-11 14:33:15 +0200

    adding test cases for containsNumbers

--- __tests__/string-validators.test.js
+++ __tests__/string-validators.test.js
@@ -29,3 +29,25 @@ describe('containsLetters', () => {
     expect(actual).toBe(true)
   })
 })
+
+describe('containsNumbers', () => {
+  it('should return false, empty string', () => {
+    const actual = containsNumbers('')
+    expect(actual).toBe(false)
+  })
+
+  it('should return false, not containing numbers', () => {
+    const actual = containsNumbers('asdfg')
+    expect(actual).toBe(false)
+  })
+
+  it('should return true, containing numbers only', () => {
+    const actual = containsNumbers('9870')
+    expect(actual).toBe(true)
+  })
+
+  it('should return true, containing letters with numbers', () => {
+    const actual = containsNumbers('qwer1234')
+    expect(actual).toBe(true)
+  })
+})
