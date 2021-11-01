8f99022310939d19b322d574affe459ea621ac61
mohamedisakr <mohamedisakr@gmail.com>
2021-11-01 06:50:35 +0200

    implementing basic graphql server

---
+++ app.graphql.js
@@ -0,0 +1,35 @@
+const {ApolloServer, gql} = require('apollo-server')
+const {personsGraphQL: persons} = require('./fixtures/persons-data')
+
+const typeDefs = gql`
+  type Person {
+    name: String!
+    phone: String
+    street: String!
+    city: String!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`
+
+const resolvers = {
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+  },
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({url}) => {
+  console.log(`Server ready at ${url}`)
+})
