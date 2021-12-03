66da07349e130f159414b92373c3d00f066b894d
mohamedisakr <mohamedisakr@gmail.com>
2021-12-03 09:41:40 +0200

    implementing blog schema test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/__tests__/**/*.test.js'],
-  testTimeout: 20000,
+// module.exports = {
+module.exports = async () => {
+  return {
+    verbose: true,
+    testEnvironment: 'node',
+    testTimeout: 20000,
+    // testMatch: [
+    //   '<rootDir>/controllers/**/*.test.js',
+    //   '<rootDir>/models/**/*.test.js',
+    //   '<rootDir>/routes/**/*.test.js',
+    //   '<rootDir>/__tests__/**/*.test.js',
+    // ],
+  }
 }
