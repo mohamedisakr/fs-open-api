952e4ae30e1b01c429446b4aca2ef8b9ffafa9af
mohamedisakr <mohamedisakr@gmail.com>
2021-12-11 09:04:58 +0200

    replacing node server with express server

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-require('dotenv').config()
-require('express-async-errors')
-const compression = require('compression')
 const express = require('express')
-const cors = require('cors')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const multer = require('multer')
-
-const app = express()
+const mainApp = express()
 
 const config = require('./utils/config')
 const homeRouter = require('./controllers/home')
@@ -19,35 +11,51 @@ const personsRouter = require('./controllers/persons')
 const middleware = require('./utils/middleware')
 // const registerRouter = require('./controllers/register')
 
-// use gzip compression - as expressjs.com suggest for production best practices
-app.use(compression())
-
-// helmet protect app from some well-known web vulnerabilities
-app.use(helmet())
-
-app.use(cors())
-app.use(morgan('combined'))
-app.use(express.static('build'))
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(multer().none())
-app.use(middleware.requestLogger)
-app.use(middleware.tokenExtractor)
+mainApp.use(middleware.requestLogger)
+mainApp.use(middleware.tokenExtractor)
 // app.use(middleware.userExtractor)
 
-app.use(homeRouter)
+mainApp.use(homeRouter)
 
 // app.use('/api/blogs', middleware.userExtractor, blogRouter)
-app.use(config.REGISTER_URL, usersRouter) //'/api/register'
-app.use(config.LOGIN_URL, loginRouter) //'/api/login'
-app.use(config.USER_URL, usersRouter) //'/api/users'
-app.use(config.BLOG_DETAILS_URL, blogRouter) //'/api/blogs/details'
-app.use(config.BLOG_URL, blogRouter) //'/api/blogs'
-app.use(config.NOTE_URL, notesRouter) //'/api/notes'
-app.use(config.PERSON_URL, personsRouter) // '/api/persons'
-app.use(config.INFO_URL, personsRouter) //'/api/info'
+mainApp.use(config.REGISTER_URL, usersRouter) //'/api/register'
+mainApp.use(config.LOGIN_URL, loginRouter) //'/api/login'
+mainApp.use(config.USER_URL, usersRouter) //'/api/users'
+mainApp.use(config.BLOG_DETAILS_URL, blogRouter) //'/api/blogs/details'
+mainApp.use(config.BLOG_URL, blogRouter) //'/api/blogs'
+mainApp.use(config.NOTE_URL, notesRouter) //'/api/notes'
+mainApp.use(config.PERSON_URL, personsRouter) // '/api/persons'
+mainApp.use(config.INFO_URL, personsRouter) //'/api/info'
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+mainApp.use(middleware.unknownEndpoint)
+mainApp.use(middleware.errorHandler)
 
-module.exports = app
+module.exports = mainApp
+
+//#region  old code
+
+// require('dotenv').config()
+// require('express-async-errors')
+// const compression = require('compression')
+// const express = require('express')
+// const cors = require('cors')
+// const morgan = require('morgan')
+// const helmet = require('helmet')
+// const multer = require('multer')
+
+// const app = express()
+
+// // use gzip compression - as expressjs.com suggest for production best practices
+// app.use(compression())
+
+// // helmet protect app from some well-known web vulnerabilities
+// app.use(helmet())
+
+// app.use(cors())
+// app.use(morgan('combined'))
+// app.use(express.static('build'))
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use(multer().none())
+
+//#endregion
--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+require('dotenv').config()
+require('express-async-errors')
+const compression = require('compression')
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const multer = require('multer')
+
+const app = express()
+const mainApp = require('./app')
+const config = require('./utils/config')
+const {info} = require('./utils/logger')
+
+// use gzip compression - as expressjs.com suggest for production best practices
+app.use(compression())
+
+// helmet protect app from some well-known web vulnerabilities
+app.use(helmet())
+
+app.use(cors())
+app.use(morgan('combined'))
+app.use(express.static('build'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(multer().none())
+
+app.use(mainApp)
+
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`)
+})
+
+/*
 const app = require('./app') // the actual Express application
 const http = require('http')
 const config = require('./utils/config')
@@ -8,3 +42,4 @@ const server = http.createServer(app)
 server.listen(config.PORT, () => {
   info(`Server running on port ${config.PORT}`)
 })
+*/
